4c12006177b89e6119cb5e08a5f05cda78fd6a6b
cerokool32 <cerokool32@gmail.com>
2016-04-26 18:03:18 -0500

    prueba de menu css

--- js/ck32_app/ck32.js
+++ js/ck32_app/ck32.js
@@ -1,5 +1,5 @@
 var ck32 = angular
-    .module('ck32', ['ngRoute', 'ngMaterial', 'ck32Theme', 'sasrio.angular-material-sidenav']);
+    .module('ck32', ['ngRoute', 'ngMaterial', 'ck32Theme']);
 
 ck32.config(function ($routeProvider) {
     $routeProvider
@@ -11,34 +11,7 @@ ck32.config(function ($routeProvider) {
         });
 });
 
-ck32.config(function (ssSideNavSectionsProvider) {
-    ssSideNavSectionsProvider.initWithSections([
-        {
-            name: 'section heading 1',
-            type: 'heading',
-            childre: [{
-                name: 'toggle 1',
-                type: 'toggle',
-                pages: [{
-                        name: 'item1',
-                        state: 'common.toogle1.item1'
-                },
-                    {
-                        name: 'item2',
-                        state: 'common.toggle2.item2'
-                }]
-            }]
-        },
-        {
-            name: 'Simple link to index state',
-            state: 'common.index',
-            type: 'link'
-        }
-
-    ]);
-});
-
-ck32.controller('BasicCtrl', function DemoCtrl($scope, $mdDialog, $mdSidenav, ssSideNav) {
+ck32.controller('BasicCtrl', function DemoCtrl($scope, $mdDialog, $mdSidenav) {
 
     $scope.toggleLeft = function () {
         $mdSidenav('left').toggle();
