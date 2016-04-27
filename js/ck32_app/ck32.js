881987640e45abbf98960b675ebdd8e4356ca916
cerokool32 <cerokool32@gmail.com>
2016-04-27 16:05:50 -0500

    Migracion de angular material a ui-bootstrap

--- js/ck32_app/ck32.js
+++ js/ck32_app/ck32.js
@@ -1,5 +1,5 @@
 var ck32 = angular
-    .module('ck32', ['ngRoute', 'ngMaterial', 'ck32Theme']);
+    .module('ck32', ['ngRoute', 'ui.bootstrap']);
 
 ck32.config(function ($routeProvider) {
     $routeProvider
@@ -11,18 +11,7 @@ ck32.config(function ($routeProvider) {
         });
 });
 
-ck32.controller('BasicCtrl', function DemoCtrl($scope, $mdDialog, $mdSidenav) {
+ck32.controller('BasicCtrl', function DemoCtrl($scope) {
 
-    $scope.toggleLeft = function () {
-        $mdSidenav('left').toggle();
-    };
 
-    $scope.action = function (name, ev) {
-        $mdDialog.show($mdDialog.alert()
-            .title(name)
-            .textContent('You triggered the "' + name + '" action')
-            .ok('Great')
-            .targetEvent(ev)
-        );
-    };
 });
