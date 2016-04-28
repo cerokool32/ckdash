5987fc6396460a03b3a64d51af672a9a6bd8c57e
cerokool32 <cerokool32@gmail.com>
2016-04-28 14:15:07 -0500

    loading overlay y labels flotantes

--- js/ck32_app/ck32.js
+++ js/ck32_app/ck32.js
@@ -1,5 +1,20 @@
 var ck32 = angular
-    .module('ck32', ['ngRoute', 'ck32_sidenav']);
+    .module('ck32', ['ngRoute', 'ck32_sidenav', 'ngAnimate', 'treasure-overlay-spinner']);
+
+ck32.run(run);
+run.$inject = ['$rootScope'];
+
+function run($rootScope) {
+    $rootScope.spinner = {
+        active: false,
+        on: function () {
+            this.active = true;
+        },
+        off: function () {
+            this.active = false;
+        }
+    };
+};
 
 ck32.config(function ($routeProvider) {
     $routeProvider
